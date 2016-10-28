49faf2a23d62de9986b47c3a70e00ec20a2669ef
Zander Martineau <zander@martineau.tv>
2016-10-28 06:20:23 +0100

    re-add lost icon font file

--- .kickoff/tasks/copy.js
+++ .kickoff/tasks/copy.js
@@ -4,7 +4,7 @@
 const gulp = require('gulp');
 const config = require('../config');
 
-gulp.task('copy', ['copy:standaloneJS' /*, 'copy:fonts'*/]);
+gulp.task('copy', ['copy:standaloneJS', 'copy:fonts']);
 
 // Copy the standalone js files
 gulp.task('copy:standaloneJS', () => {
@@ -13,7 +13,7 @@ gulp.task('copy:standaloneJS', () => {
 });
 
 // Copy webfonts
-// gulp.task('copy:fonts', () => {
-// 	return gulp.src(`${config.srcDir}/fonts/*`)
-// 		.pipe(gulp.dest(`${config.distDir}/fonts`));
-// });
+gulp.task('copy:fonts', () => {
+	return gulp.src(`${config.srcDir}/fonts/*`)
+		.pipe(gulp.dest(`${config.distDir}/fonts`));
+});
