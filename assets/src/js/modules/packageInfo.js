2a166e85679be20ab0c94f3e23399448a879de54
Zander Martineau <zander@martineau.tv>
2016-11-15 20:22:31 +0000

    added releases nav item to allow for easy access to kickoff-related modules

--- assets/src/js/modules/packageInfo.js
+++ assets/src/js/modules/packageInfo.js
@@ -1,42 +1,42 @@
-var $ = require('jquery');
+import axios from 'axios';
+
+// List of packages to be included
+const packages = [
+	'kickoff',
+	'generator-kickoff',
+	'kickoff-grid.css',
+	'kickoff-utils.scss',
+	'kickoff-fluidVideo.css',
+	'kickoff-snippets',
+]
+
+let packageEndpoints = [];
 
 function init() {
-	if ($('.currentVersion').length) {
-		getPackageJSON();
-	}
-}
-
-function getPackageJSON() {
-	$.ajax({
-		url: 'https://rawgit.com/tmwagency/kickoff/master/package.json',
-		type: 'GET',
-		dataType: 'json',
-	})
-	.done(function(data) {
-		// console.log("success", data);
-
-		setVersion(data.version);
+	packages.forEach(pkg => {
+		packageEndpoints.push(axios.get(`https://rawgit.com/TryKickoff/${pkg}/master/package.json`));
 	});
-}
 
-function setVersion(version) {
-	$('.currentVersion').text(version);
-}
+	// If you add a package to the array above,
+	// ensure you add its data as an argument here
+	axios.all(packageEndpoints).then(axios.spread((kickoffData, generatorData, gridData, utilsData, fluidVideoData, snippetsData) => {
+		// console.log(kickoffData, generatorData, gridData, utilsData, fluidVideoData, snippetsData)
 
-function getLatestCommit() {
-	$.ajax({
-		url: 'https://api.github.com/repos/tmwagency/kickoff/commits',
-		type: 'GET',
-		dataType: 'json',
-	})
-	.done(function(data) {
-		console.log("success", data[0].commit);
-		var commitText = 'Latest commit: "' + data[0].commit.message + '" by ' + data[0].commit.author.name;
-		var commitUrl = data[0].commit.url.replace('api.', '').replace('repos/', '').replace('git/', '').replace('commits', 'commit');
+		// Manually add the button using the data from 'ajax' :wink:
+		document.querySelector('.nav-releases').innerHTML = `
+			<a class="nav-subItem" href="https://github.com/TryKickoff/${kickoffData.data.name}/">${kickoffData.data.name}@${kickoffData.data.version}</a>
+			<a class="nav-subItem" href="https://github.com/TryKickoff/${generatorData.data.name}/">${generatorData.data.name}@${generatorData.data.version}</a>
+			<a class="nav-subItem" href="https://github.com/TryKickoff/${gridData.data.name}/">${gridData.data.name}@${gridData.data.version}</a>
+			<a class="nav-subItem" href="https://github.com/TryKickoff/${utilsData.data.name}/">${utilsData.data.name}@${utilsData.data.version}</a>
+			<a class="nav-subItem" href="https://github.com/TryKickoff/${fluidVideoData.data.name}/">${fluidVideoData.data.name}@${fluidVideoData.data.version}</a>
+			<a class="nav-subItem" href="https://github.com/TryKickoff/${snippetsData.data.name}/">${snippetsData.data.name}@${snippetsData.data.version}</a>
+		`;
 
-		$('.latestCommit span').text(commitText);
-		$('.latestCommit a').attr('href', commitUrl);
-	});
+		// Add the version to the home page hero area
+		if (document.querySelector('.currentVersion--new')) {
+			document.querySelector('.currentVersion--new').textContent = kickoffData.data.version;
+		}
+	}));
 }
 
 module.exports = init;
