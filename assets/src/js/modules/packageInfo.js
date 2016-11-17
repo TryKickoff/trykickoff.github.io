8c002aebd47608ad189d3849021bec2db7a137c1
Zander Martineau <zander@martineau.tv>
2016-11-17 17:47:29 +0000

    add target:blank to the release links

--- assets/src/js/modules/packageInfo.js
+++ assets/src/js/modules/packageInfo.js
@@ -25,13 +25,13 @@ function init() {
 
 		// Manually add the button using the data from 'ajax' :wink:
 		document.querySelector('.nav-releases').innerHTML = `
-			<a class="nav-subItem" href="https://github.com/TryKickoff/${kickoffData.data.name}/">${kickoffData.data.name}@${kickoffData.data.version}</a>
-			<a class="nav-subItem" href="https://github.com/TryKickoff/${generatorData.data.name}/">${generatorData.data.name}@${generatorData.data.version}</a>
-			<a class="nav-subItem" href="https://github.com/TryKickoff/${gridData.data.name}/">${gridData.data.name}@${gridData.data.version}</a>
-			<a class="nav-subItem" href="https://github.com/TryKickoff/${utilsData.data.name}/">${utilsData.data.name}@${utilsData.data.version}</a>
-			<a class="nav-subItem" href="https://github.com/TryKickoff/${fluidVideoData.data.name}/">${fluidVideoData.data.name}@${fluidVideoData.data.version}</a>
-			<a class="nav-subItem" href="https://github.com/TryKickoff/${snippetsData.data.name}/">${snippetsData.data.name}@${snippetsData.data.version}</a>
-			<a class="nav-subItem" href="https://github.com/TryKickoff/${statixData.data.name}/">${statixData.data.name}@${statixData.data.version}</a>
+			<a class="nav-subItem" href="https://github.com/TryKickoff/${kickoffData.data.name}/" target="_blank">${kickoffData.data.name}@${kickoffData.data.version}</a>
+			<a class="nav-subItem" href="https://github.com/TryKickoff/${generatorData.data.name}/" target="_blank">${generatorData.data.name}@${generatorData.data.version}</a>
+			<a class="nav-subItem" href="https://github.com/TryKickoff/${gridData.data.name}/" target="_blank">${gridData.data.name}@${gridData.data.version}</a>
+			<a class="nav-subItem" href="https://github.com/TryKickoff/${utilsData.data.name}/" target="_blank">${utilsData.data.name}@${utilsData.data.version}</a>
+			<a class="nav-subItem" href="https://github.com/TryKickoff/${fluidVideoData.data.name}/" target="_blank">${fluidVideoData.data.name}@${fluidVideoData.data.version}</a>
+			<a class="nav-subItem" href="https://github.com/TryKickoff/${snippetsData.data.name}/" target="_blank">${snippetsData.data.name}@${snippetsData.data.version}</a>
+			<a class="nav-subItem" href="https://github.com/TryKickoff/${statixData.data.name}/" target="_blank">${statixData.data.name}@${statixData.data.version}</a>
 		`;
 
 		// Add the version to the home page hero area
