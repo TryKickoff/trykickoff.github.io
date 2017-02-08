35b68aaf938c65892ccb376afff0dfd94d7b434a
Zander Martineau <zander@martineau.tv>
2017-02-08 17:11:36 +0000

    cleanup js

--- assets/src/js/modules/packageInfo.js
+++ assets/src/js/modules/packageInfo.js
@@ -14,34 +14,29 @@ const packages = [
 
 let packageEndpoints = [];
 
-function init() {
+export default function () {
 	packages.forEach(pkg => {
 		packageEndpoints.push(axios.get(`https://rawgit.com/TryKickoff/${pkg}/master/package.json`));
 	});
 
 	axios.all(packageEndpoints).then((response) => {
-		console.log(response);
 		let html = ``;
-
 		response.forEach(item => {
+			const name = item.data.name === 'kickoff-framework' ? 'kickoff' : item.data.name;
 			html += `
 				<a class="nav-subItem release"
-					title="Visit the ${item.data.name} repo"
-					href="https://github.com/TryKickoff/${item.data.name}/"
+					title="Visit the ${name} repo"
+					href="https://github.com/TryKickoff/${name}/"
 					target="_blank">
-						<span class="release-name">${item.data.name}</span>
+						<span class="release-name">${name}</span>
 						<span class="release-version">${item.data.version}</span>
 				</a>`;
-
 				// Add the version to the home page hero area
-				if (document.querySelector('.currentVersion--new') && item.data.name === 'kickoff') {
+				if (document.querySelector('.currentVersion--new') && name === 'kickoff') {
 					document.querySelector('.currentVersion--new').textContent = item.data.version;
 				}
 		});
 
 		document.querySelector('.nav-releases').innerHTML = html;
-
 	});
 }
-
-module.exports = init;
